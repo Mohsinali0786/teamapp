34204d50dddd174701c5438c3a27d252c47f88fe
Mohsin Muhammad Ali <56477672+Mohsinali0786@users.noreply.github.com>
2022-10-14 12:14:10 +0500

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import SignupForm from './screen/signup';
+
+function App() {
+  return (
+    <div className="App">
+      <SignupForm/>
+    </div>
+  );
+}
+
+export default App;
