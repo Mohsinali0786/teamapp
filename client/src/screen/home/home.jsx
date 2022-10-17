1cad6c2d606c256790034bdbe387a2546f748357
Mohsin Muhammad Ali <56477672+Mohsinali0786@users.noreply.github.com>
2022-10-17 15:52:14 +0500

    updated home v1

--- client/src/screen/home/home.jsx
+++ client/src/screen/home/home.jsx
@@ -1,12 +1,12 @@
 import { useSelector } from "react-redux"
-
-
+import AddIcon from '@mui/icons-material/Add';
 export default function Home(){
     const dataRedux=useSelector((state)=>state)
     console.log('dataRedux',dataRedux)
     return(
         <div>
             <h2>Home</h2>
+            <AddIcon className='add-team-icon'/>
         </div>
     )
 }
